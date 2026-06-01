17f854a942f573bf4fa4d06fa2b058c8bcebd5e0
Shahd Henagl <shahdhenagl19@gmail.com>
2026-06-01 11:21:01 +0300

    Add explicit Vercel collection API routes

---
+++ api/collections/[collection].js
@@ -0,0 +1,9 @@
+import { handleCollection, sendJson } from '../_mongo.js';
+
+export default async function handler(req, res) {
+  try {
+    return handleCollection(req, res, req.query.collection);
+  } catch (error) {
+    return sendJson(res, 500, { error: error.message || 'Internal server error' });
+  }
+}
